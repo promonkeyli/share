a99f749b47446ec926292d9476773e82e1cedf77
Young <liyang_email@163.com>
2022-04-19 21:56:12 +0800

    weekly add

--- src/pages/information/index.tsx
+++ src/pages/information/index.tsx
@@ -11,6 +11,17 @@ function Information(){
             </div>
             {/*<div style={{padding:'10px'}}><div className={styles.rankBox}>编程语言排行榜</div></div>*/}
             {/*<div style={{padding: '10px'}}><div className={styles.magazineBox}>前端刊物</div></div>*/}
+            <div className={styles.magazineBox}>
+                <div>
+                    <span>前端周刊推荐</span>
+                    <ul>
+                        <li><label>前端精读周刊</label><a href="https://github.com/ascoders/weekly" target='_blank'>https://github.com/ascoders/weekly</a></li>
+                        <li><label>科技爱好者周刊</label><a href="https://github.com/ruanyf/weekly" target='_blank'>https://github.com/ruanyf/weekly</a></li>
+                        <li><label>前端好文汇总</label><a href="https://github.com/zenany/weekly" target='_blank'>https://github.com/zenany/weekly</a></li>
+                        <li><label>阿里云前端技术周刊</label><a href="https://github.com/aliyunfe/weekly" target='_blank'>https://github.com/aliyunfe/weekly</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }

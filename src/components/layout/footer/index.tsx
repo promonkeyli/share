93475eb0cebd85be1a072e1b27bb953bac58a4fb
Young <liyang_email@163.com>
2022-07-03 13:00:17 +0800

    公安备案号添加

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,7 +1,15 @@
 function Footer(){
     return (
         <div style={{color: '#444'}}>
-            @2022 share the knowledge of front-end | design by Young | 蜀ICP备2022005564号
+            <p>@2022 share the knowledge of front-end | design by Young</p>
+            <p>
+                蜀ICP备2022005564号 |
+                <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51082402000177"
+                style={{ textDecoration: "none", color: "#444", marginLeft: '10px' }}>
+                    <img src="	https://www.beian.gov.cn/img/ghs.png" alt='' style={{ verticalAlign: 'middle' }}/>
+                    <span style={{marginLeft: '5px'}}>川公网安备 51082402000177号</span>
+                </a>
+            </p>
         </div>
     )
 }

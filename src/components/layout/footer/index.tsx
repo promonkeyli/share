f7942e43aac4f9540ebb508b3d984a7f7e6bb476
Young <liyang_email@163.com>
2022-11-06 13:24:49 +0800

    ICP工信部备案跳转链接信息添加

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -3,7 +3,9 @@ function Footer(){
         <div style={{color: '#444'}}>
             <p>@2022 share the knowledge of front-end | design by Young</p>
             <p>
-                蜀ICP备2022005564号 |
+                <a target="_blank"
+                   style={{ textDecoration: "none", color: "#444", marginLeft: '10px' }}
+                   href="https://beian.miit.gov.cn/">蜀ICP备2022005564号</a> |
                 <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51082402000177"
                 style={{ textDecoration: "none", color: "#444", marginLeft: '10px' }}>
                     <img src="	https://www.beian.gov.cn/img/ghs.png" alt='' style={{ verticalAlign: 'middle' }}/>
